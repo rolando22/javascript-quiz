748ab310605b2ed99d6a859e05dabdb7c84d7d33
rolando22 <rolando2207@gmail.com>
2023-08-28 15:32:22 -0300

    [ADD] API_URL dinámica

--- src/services/getQuestions.ts
+++ src/services/getQuestions.ts
@@ -1,7 +1,9 @@
 import { Question } from "../types/question";
 
+const API_URL = import.meta.env.PROD ? 'https://rolando22.github.io/javascript-quiz/' : 'http://localhost:5173/'
+
 export async function getQuestions(limit: number) {
-    const res = await fetch('http://localhost:5173/questions.json');
+    const res = await fetch(`${API_URL}questions.json`);
     const data: Question[] = await res.json();
     const questions = data.sort(() => Math.random() - 0.5).slice(0, limit);
     return questions;
